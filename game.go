38f5c7178d97af2966da08de43d0f4ae243ec143
Ralph Bodenner <rkbodenner+github@gmail.com>
2014-05-29 22:05:47 -0700

    Some steps must be done by each player

--- game.go
+++ game.go
@@ -5,19 +5,24 @@ import (
 )
 
 type SetupRules struct {
-  Steps []string
+  Steps []SetupStep
+}
+
+type SetupStep struct {
+  Description string
+  Arity string
 }
 
 var TicTacToeSetup = SetupRules{
-  []string{
-    "Draw 3x3 grid",
-    "Choose X or O",
+  []SetupStep{
+    {"Draw 3x3 grid", "Once"},
+    {"Choose X or O", "Each player"},
   },
 }
 
 func main() {
   fmt.Println("game on")
   for _,r := range TicTacToeSetup.Steps {
-    fmt.Println(r)
+    fmt.Printf("%s\t%s\n", r.Description, r.Arity)
   }
 }
