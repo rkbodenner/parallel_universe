d9a4332fa7ce940d081f1b12da4d2eaf2581b3f5
Ralph Bodenner <rkbodenner+github@gmail.com>
2014-05-29 22:49:10 -0700

    Method for printing steps, add another game for testing

--- game.go
+++ game.go
@@ -9,6 +9,19 @@ type Game struct {
   PlayerCount int
 }
 
+func (game *Game) PrintSteps() (error) {
+  for _,r := range game.Setup.Steps {
+    if "Each player" == r.Arity {
+      for i := 0; i < game.PlayerCount; i++ {
+        fmt.Printf("%s\tPlayer %d\n", r.Description, i)
+      }
+    } else {
+      fmt.Printf("%s\t%s\n", r.Description, r.Arity)
+    }
+  }
+  return nil
+}
+
 type SetupRules struct {
   Steps []SetupStep
 }
@@ -18,27 +31,46 @@ type SetupStep struct {
   Arity string
 }
 
-var TicTacToeSetup = SetupRules{
-  []SetupStep{
-    {"Draw 3x3 grid", "Once"},
-    {"Choose X or O", "Each player"},
-  },
+func NewTicTacToe() *Game {
+  var setup = SetupRules{
+    []SetupStep{
+      {"Draw 3x3 grid", "Once"},
+      {"Choose X or O", "Each player"},
+    },
+  }
+
+  var game = Game{
+    Setup: &setup,
+    PlayerCount: 2,
+  }
+
+  return &game
 }
 
-var TicTacToe = Game{
-  Setup: &TicTacToeSetup,
-  PlayerCount: 2,
+func NewForbiddenIsland() *Game {
+  var setup = SetupRules{
+    []SetupStep{
+      {"Create Forbidden Island", "Once"},
+      {"Place the treasures", "Once"},
+      {"Divide the cards", "Once"},
+      {"The island starts to sink", "Once"},
+      {"Deal Adventurer cards", "Once"},
+      {"Place Adventurer pawn", "Each player"},
+      {"Hand out Treasure deck cards", "Once"},
+      {"Set the water level", "Once"},
+    },
+  }
+
+  var game = Game{
+    Setup: &setup,
+    PlayerCount: 2,
+  }
+
+  return &game
 }
 
 func main() {
   fmt.Println("game on")
-  for _,r := range TicTacToe.Setup.Steps {
-    if "Each player" == r.Arity {
-      for i := 0; i < TicTacToe.PlayerCount; i++ {
-        fmt.Printf("%s\tPlayer %d\n", r.Description, i)
-      }
-    } else {
-      fmt.Printf("%s\t%s\n", r.Description, r.Arity)
-    }
-  }
+  game := NewForbiddenIsland()
+  game.PrintSteps()
 }
