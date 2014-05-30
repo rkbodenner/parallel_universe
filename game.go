21eb4d480657b75e8335a741afce3eac2406ad46
Ralph Bodenner <rkbodenner+github@gmail.com>
2014-05-29 21:53:41 -0700

    Setup rules

--- game.go
+++ game.go
@@ -4,6 +4,20 @@ import (
   "fmt"
 )
 
+type SetupRules struct {
+  Steps []string
+}
+
+var TicTacToeSetup = SetupRules{
+  []string{
+    "Draw 3x3 grid",
+    "Choose X or O",
+  },
+}
+
 func main() {
-  fmt.Println("game on")  
+  fmt.Println("game on")
+  for _,r := range TicTacToeSetup.Steps {
+    fmt.Println(r)
+  }
 }
