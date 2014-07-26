a203dc015ceaf342ee91870e80e1e543e3759249
Ralph Bodenner <rkbodenner+github@gmail.com>
2014-07-25 22:33:49 -0700

    Placeholder for setup rules details

--- game/setup_rule.go
+++ game/setup_rule.go
@@ -7,12 +7,15 @@ import (
 type SetupRule struct {
   Id int
   Description string
+  Details string
   Arity string
   Dependencies []*SetupRule
 }
 
+const IPSUM string = "You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man."
+
 func NewSetupRule(desc string, arity string, deps ...*SetupRule) *SetupRule {
-  return &SetupRule{0, desc, arity, deps}
+  return &SetupRule{0, desc, IPSUM, arity, deps}
 }
 
 type SetupStep struct {
