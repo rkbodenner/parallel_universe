683372100de32a40b0bb05099c6c061d14e54fb5
Ralph Bodenner <rkbodenner+github@gmail.com>
2014-08-04 00:00:31 -0700

    graph loads games optionally from a database

--- graph/graph.go
+++ graph/graph.go
@@ -9,7 +9,12 @@ go run graph.go | dot -Tpng > game.png
 package main
 
 import (
+  "database/sql"
+  "flag"
   "fmt"
+  "os"
+  _ "github.com/lib/pq"
+  "github.com/rkbodenner/meeple_mover/record"
   "github.com/rkbodenner/parallel_universe/collection"
   "github.com/rkbodenner/parallel_universe/game"
 )
@@ -26,5 +31,64 @@ func PrintDot(g *game.Game) {
 }
 
 func main() {
-  PrintDot(collection.NewOraEtLaboraShortMultiplayer())
+  var gameName string
+  flag.StringVar(&gameName, "game", "", "Name of the game to print a dot file for")
+  var useTestDB bool
+  flag.BoolVar(&useTestDB, "test-db", false, "Load games from the test database")
+  var verbose bool
+  flag.BoolVar(&verbose, "verbose", false, "Print messages about progress to stdout")
+
+  flag.Parse()
+  if "" == gameName {
+    flag.Usage()
+    os.Exit(1)
+  }
+
+  if verbose {
+    fmt.Printf("Searching for %s...\n", gameName)
+  }
+  var game *game.Game
+
+  if useTestDB {
+    if verbose {
+      fmt.Println("Loading from the test database...")
+    }
+
+    connectString := fmt.Sprintf("user=ralph dbname=meeple_mover_test sslmode=disable")
+    db, err := sql.Open("postgres", connectString)
+    if nil != err {
+      fmt.Print(err)
+    }
+    defer db.Close()
+
+    rec := record.NewEmptyGameRecord()
+    // FIXME: This prints messages to stdout, which breaks redirecting output straight to a dot file
+    err = rec.FindByName(db, gameName)
+    if nil != err {
+      fmt.Println(err)
+    } else {
+      game = rec.Game
+    }
+  } else {
+    if verbose {
+      fmt.Println("Loading from the collection...")
+    }
+
+    shelf := collection.NewCollection().Games
+    for _, g := range shelf {
+      if gameName == g.Name {
+        if verbose {
+          fmt.Printf("Found %s in the collection\n", gameName)
+        }
+        game = g
+        break
+      }
+    }
+  }
+
+  if nil != game {
+    PrintDot(game)
+  } else {
+    fmt.Fprintf(os.Stderr, "Error: No such game")
+  }
 }
